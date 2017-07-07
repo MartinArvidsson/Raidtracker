c64e7db9d57d96422d7091be637b530776b94bf5
MartinArvidsson <Martinarvidsson1994@gmail.com>
2017-07-07 13:27:51 +0200

    Works, finetuning inc.

--- public/js/Table.js
+++ public/js/Table.js
@@ -1,14 +1,28 @@
-var app = angular.module('myApp', []);
+'use strict'
+const app = angular.module('myApp', []);
+const _mythicKills = {};
 
-app.controller('raiders', ['$scope', function ($scope) {
+app.controller('raidController', ['$scope', '$http', 'apiService', function ($scope, $http, apiService) {
     $scope.raiders = [
-        { Name: "Jedslims", Guild: "FatsharkYes", Region: "EU" }, { Name: "Brenwyn", Guild: "Solaris", Region: "EU" }, { Name: "Nerves", Guild: "Solaris", Region: "EU" },
-        { Name: "Proximitynz", Guild: "One", Region: "US" }, { Name: "Daarshan", Guild: "Club Camel", Region: "US" }, { Name: "Hula", Guild: "Happy Accidents", Region: "US" },
-        { Name: "Moofz", Guild: "Happy Accidents", Region: "US" }, { Name: "Dal", Guild: "Death Jesters", Region: "US" },
-        { Name: "Bananbrus", Guild: "Familiar With Drama", Region: "EU" }, { Name: "Ghost", Guild: "Lol guildess", Region: "EU" }, { Name: "Putro", Guild: "Phoenix", Region: "EU" },
-        { Name: "Wobberc", Guild: "Phoenix", Region: "EU" }, { Name: "Artiks", Guild: "Phoenix", Region: "EU" }, { Name: "Shizzlee", Guild: "Grave", Region: "EU" },
-        { Name: "Mayco", Guild: "Washed Up", Region: "US" }, { Name: "Pudgypew", Guild: "Bad Alts", Region: "EU" }, { Name: "Gorno", Guild: "Bad Alts", Region: "EU" },
-        { Name: "Faith", Guild: "Infallible", Region: "US" }, { Name: "Shyáel", Guild: "Warm", Region: "EU" }
+        { Name: "Jedslims", Character: "Jedslims", Guild: "FatsharkYes", Region: "EU", Realm: "Kazzak" },
+        { Name: "Brenwyn", Character: "Brenwyn", Guild: "Solaris", Region: "EU", Realm: "Ragnaros" },
+        { Name: "Nerves", Character: "Neerves", Guild: "Solaris", Region: "EU", Realm: "Ragnaros" },
+        { Name: "Proximitynz", Character: "Proximitynz", Guild: "One", Region: "US", Realm: "Frostmourne" },
+        { Name: "Daarshan", Character: "Daarshan", Guild: "Club Camel", Region: "US", Realm: "Stormrage" },
+        { Name: "Hula", Character: "Hulamom", Guild: "Happy Accidents", Region: "US", Realm: "Area-52" },
+        { Name: "Moofz", Character: "Moofz", Guild: "Happy Accidents", Region: "US", Realm: "Area-52" },
+        { Name: "Dal", Character: "Delcenti", Guild: "Death Jesters", Region: "US", Realm: "Stormrage" },
+        { Name: "Bananbrus", Character: "Bananbrus", Guild: "Familiar With Drama", Region: "EU", Realm: "Ravencrest" },
+        { Name: "Ghost", Character: "Ghosteld", Guild: "Lol guildless", Region: "EU", Realm: "Sunstrider" },
+        { Name: "Putro", Character: "Putrohunt", Guild: "Phoenix", Region: "EU", Realm: "Silvermoon" },
+        { Name: "Wobberc", Character: "Wobberc", Guild: "Phoenix", Region: "EU", Realm: "Silvermoon" },
+        { Name: "Artiks", Character: "Artiks", Guild: "Phoenix", Region: "EU", Realm: "Silvermoon" },
+        { Name: "Shizzlee", Character: "Shizzlee", Guild: "Grave", Region: "EU", Realm: "Draenor" },
+        { Name: "Mayco", Character: "Emlis", Guild: "Washed Up", Region: "US", Realm: "Saurfang" },
+        { Name: "Pudgypew", Character: "Pudgypew", Guild: "Bad Alts", Region: "EU", Realm: "Silvermoon" },
+        { Name: "Gorno", Character: "Gorno", Guild: "Bad Alts", Region: "EU", Realm: "Silvermoon" },
+        { Name: "Faith", Character: "Wrekedrood", Guild: "Infallible", Region: "US", Realm: "Whisperwind" },
+        { Name: "Shyáel", Character: "Shyáel", Guild: "Warm", Region: "EU", Realm: "Silvermoon" }
     ];
 
     $scope.headers = [
@@ -27,63 +41,77 @@ app.controller('raiders', ['$scope', function ($scope) {
         "Total kills:"
     ];
 
-    $scope.mythicKills = {
+    $scope.mythicKills = [];
 
-    };
-/*$scope.mythicKills = function(){
-        var _mythicKills =  {};
-        for (var raider in $scope.raiders) {
-            _mythicKills[raider.Name] = tosKills(raider.Name,raider.Server,raider.Region)
+    $scope.calcKills = function (responseData) {
+        const playerAch = responseData.achievements.achievementsCompleted;
+        for (var i = 0; i < $scope.raiders.length; i++) {
+            console.log(responseData.name)
+            if (responseData.name === $scope.raiders[i].Character) {
+
+                if (playerAch.includes(11767)) {
+                    $scope.raiders[i]["goroth"] = true;
+                }
+                if (playerAch.includes(11774)) {
+                    $scope.raiders[i]["demonic"] = true;
+                }
+                if (playerAch.includes(11775)) {
+                    $scope.raiders[i]["harjatan"] = true;
+                }
+                if (playerAch.includes(11776)) {
+                    $scope.raiders[i]["sisters"] = true;
+                }
+                if (playerAch.includes(11777)) {
+                    $scope.raiders[i]["mistress"] = true;
+                }
+                if (playerAch.includes(11778)) {
+                    $scope.raiders[i]["host"] = true;
+                }
+                if (playerAch.includes(11779)) {
+                    $scope.raiders[i]["maiden"] = true;
+                }
+                if (playerAch.includes(11780)) {
+                    $scope.raiders[i]["avatar"] = true;
+                }
+                if (playerAch.includes(11781)) {
+                    $scope.raiders[i]["kj"] = true;
+                }
+            }
         }
-}*/
-    
+        console.log($scope.raiders);
+    };
 
+    const init = function () {
+        for (var i = 0; i < $scope.raiders.length; i++) {
+            apiService.getAchivements($scope.raiders[i].Character, $scope.raiders[i].Realm, $scope.raiders[i].Region).then(function (data) {
+                $scope.calcKills(data)
+            }), function (error) {
+                console.log(error);
+            }
+        }
+    };
+    init();
 }]);
 
-/*var tosKills = function(Name,Server,Region){
-    var totalkills = {
-        person: name,
-        gorothKilled = false,
-        demonicKilled = false,
-        harjatanKilled = false,
-        sistersKilled = false,
-        mistressKilled = false,
-        hostKilled = false,
-        maidenKilled = false,
-        avatarKilled = false,
-        kiljaedenKilled = false
+app.factory('apiService', function ($http, $q) {
+    return {
+        getAchivements: function (Character, Realm, Region) {
+            // the $http API is based on the deferred/promise APIs exposed by the $q service
+            // so it returns a promise for us by default
+            return $http.get('https://' + Region + '.api.battle.net/wow/character/' + Realm + '/' +
+                Character + '?fields=achievements&locale=en_GB&apikey=rc8fa2wgkks6f9tffd96hmgq3qwzmuhn')
+                .then(function (response) {
+                    if (typeof response.data === 'object') {
+                        return response.data;
+                    } else {
+                        // invalid response
+                        return $q.reject(response.data);
+                    }
+
+                }, function (response) {
+                    // something went wrong
+                    return $q.reject(response.data);
+                });
+        }
     };
-
-    var response = UrlFetchApp.fetch('https://'+Region+'.api.battle.net/wow/character/'+Server+'/'+Name+'?fields=achievements&locale=en_GB&apikey=rc8fa2wgkks6f9tffd96hmgq3qwzmuhn');
-    var dataAll = JSON.parse(response);
-    var data = dataAll.achievements.achievementsCompleted;
-     if(isInArray(11767,data) === true){
-         totalkills.gorothKilled = true;
-     }
-     if(isInArray(11774,data) === true){
-         totalkills.demonicKilled = true;
-     }
-     if(isInArray(11775,data) === true){
-         totalkills.harjatanKilled = true;
-     }
-     if(isInArray(11776,data) === true){
-         totalkills.sistersKilled = true;
-     }
-     if(isInArray(11777,data) === true){
-         totalkills.mistressKilled = true;
-     }
-     if(isInArray(11778,data) === true){
-         totalkills.hostKilled = true;
-     }
-     if(isInArray(11779,data) === true){
-         totalkills.maidenKilled = true;
-     }
-     if(isInArray(11780,data) === true){
-         totalkills.avatarKilled = true;
-     }
-     if(isInArray(11781,data) === true){
-         totalkills.kiljaedenKilled = true;
-     }
-
-     return totalkills;
-}*/
+});
